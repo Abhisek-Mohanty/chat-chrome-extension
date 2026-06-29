3c3d40e2b2da780131442bf2870b70c81620cf22
Abhisek-Mohanty <abhisekm048@gmail.com>
2026-06-29 22:42:51 +0530

    socket io import

---
+++ api/fetchLocation.js
@@ -0,0 +1,18 @@
+// import { io } from "https://cdn.socket.io/4.7.4/socket.io.esm.min.js\";
+
+// const socket = io("https://your-server.com\");
+
+// socket.on(\"connect\", () => {
+//   console.log(\"Connected as \" + socket.id);
+// });
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "emit_event") {
+//   socket.emit(message.event, message.data);
+// }
+// });
+
+// socket.on("server_response", (data) => {
+//   chrome.runtime.sendMessage({
+//   type: "server_response", data });
+// });// });
--- background.js
+++ background.js
@@ -1,7 +1,14 @@
+import { io } from "./socket.io.esm.min.js";
+
 chrome.runtime.onInstalled.addListener(details => {
     console.log(details);
-})
+});
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message);
+    // console.log("hii", io);
 });
+
+
+
+
